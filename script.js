22b903b795afb3c9cbf4537ca10fd964476a9d97
ganeshnitalikar <ganeshnitalikar@gmail.com>
2023-08-04 00:49:52 +0530

    Initital Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+    // const form = document.getElementById('contact-form');
+
+    // form.addEventListener('submit', (e) => {
+    //     e.preventDefault();
+
+    //     const formData = new FormData(form);
+    //     const req = new XMLHttpRequest();
+    //     req.open('POST', form.action, true);
+    //     req.setRequestHeader('Accept', 'application/json');
+    //     req.onreadystatechange = function () {
+    //         if (req.readyState === XMLHttpRequest.DONE) {
+    //             if (req.status === 200) {
+    //                 alert('Message sent successfully!');
+    //                 form.reset();
+    //             } else {
+    //                 alert('Oops! Something went wrong.');
+    //             }
+    //         }
+    //     };
+    //     req.send(formData);
+    // });
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+    function typeWriter(text, i, fnCallback) {
+        if (i < text.length) {
+            document.getElementById("typed-text").innerHTML += text.charAt(i);
+            i++;
+            setTimeout(function () {
+                typeWriter(text, i, fnCallback);
+            }, 100);
+        } else {
+            fnCallback();
+        }
+    }
+
+    function startTyping() {
+        const text = "Ganesh Nitalikar";
+        const i = 0;
+        document.getElementById("typed-text").innerHTML = "";
+        typeWriter(text, i, function () {});
+    }
+
+    document.addEventListener('DOMContentLoaded', startTyping);
