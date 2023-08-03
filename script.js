bdfe97846deaf192880f1ac56c1227f713520544
ganeshnitalikar <ganeshnitalikar@gmail.com>
2023-08-04 01:08:02 +0530

    garbage code removed

--- script.js
+++ script.js
@@ -26,25 +26,3 @@ function startTyping() {
 
 document.addEventListener("DOMContentLoaded", startTyping);
 
-
-const form = document.getElementById('contact-form');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const formData = new FormData(form);
-    const req = new XMLHttpRequest();
-    req.open('POST', form.action, true);
-    req.setRequestHeader('Accept', 'application/json');
-    req.onreadystatechange = function () {
-        if (req.readyState === XMLHttpRequest.DONE) {
-            if (req.status === 200) {
-                alert('Message sent successfully!');
-                form.reset();
-            } else {
-                alert('Oops! Something went wrong.');
-            }
-        }
-    };
-    req.send(formData);
-});
